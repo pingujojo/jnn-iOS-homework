d35a38b2d98d6c2b1dfc5a3b2ab3c5ceab4135e7
pingujojo <penguin00924@gmail.com>
2014-05-31 00:12:22 +0800

    [CardGame] Lecture-1: Move files to certain folder.

---
+++ CardGame/CardGame/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  CardGame
+//
+//  Created by Chung-Chan Wu on 2014/5/30.
+//  Copyright (c) 2014年 Chung-Chan Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ CardGame/CardGame/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  CardGame
+//
+//  Created by Chung-Chan Wu on 2014/5/30.
+//  Copyright (c) 2014年 Chung-Chan Wu. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
